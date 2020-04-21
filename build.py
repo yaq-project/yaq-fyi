cba8d7d9c50e33ac541991fd2ed757ad9dfda67e
Blaise Thompson <blaise@untzag.com>
2020-04-21 13:37:56 -0500

    remove protocol page

--- build.py
+++ build.py
@@ -25,7 +25,7 @@ with open(p, 'w') as fh:
 
 # pages without arguments -------------------------------------------------------------------------
 
-names = ["protocol", "introduction", "licensing", "glossary", "contact"]
+names = ["introduction", "licensing", "glossary", "contact"]
 
 for name in names:
 
