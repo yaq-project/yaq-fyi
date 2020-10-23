dc914130324f057364845c8299cf5481b5985e9d
Kyle Sunden <git@ksunden.space>
2020-10-22 20:24:21 -0500

    Render from avpr origin for both traits and daemons

--- graph.py
+++ graph.py
@@ -20,59 +20,30 @@ class Trait(object):
         self.trait = kwargs["trait"]
         self.name = self.trait
         self.doc = kwargs["doc"]
-        self.requires = dict()
-        for r in kwargs["requires"]:
-            self.requires[r] = []
+        self.requires = kwargs["requires"]
         self.config = kwargs.get("config", dict())
         self.state = kwargs.get("state", dict())
         self.messages = kwargs.get("messages", dict())
-        self.daemons = dict()
+        self.daemons = []
 
     def __repr__(self):
         return self.name
 
-    def format_origin(self, lis):
-        return "from " + " ➜ ".join([f"<a href='../{l}'>{l}</a>" for l in lis])
+    def format_origin(self, origin):
+        return f"from <a href='../{origin}'>{origin}</a>"
 
 
 traits = {}
 
 # initialize
 for name in os.listdir(__here__ / "traits"):
-    dic = toml.load(__here__ / "traits" / name)
+    with open(__here__ / "traits" / name) as f:
+        dic = json.load(f)
     t = Trait(**dic)
     traits[t.trait] = t
 
 # sort
 traits = collections.OrderedDict(sorted(traits.items()))
-
-# populate
-todo = []
-for t in traits.values():
-    for k, v in t.requires.items():
-        todo.append((t, k, v))
-while todo:
-    t, k, v = todo.pop(0)
-    if k not in t.requires.keys():
-        t.requires[k] = v
-    # config
-    for name, config in traits[k].config.items():
-        if name not in t.config.keys():
-            t.config[name] = copy.deepcopy(config)
-            t.config[name]["origin"] = v + [k] + config.get("origin", [])
-    # state
-    for name, state in traits[k].state.items():
-        if name not in t.state.keys():
-            t.state[name] = copy.deepcopy(state)
-            t.state[name]["origin"] = v + [k] + state.get("origin", [])
-    # method
-    for name, message in traits[k].messages.items():
-        if name not in t.messages.keys():
-            t.messages[name] = copy.deepcopy(message)
-            t.messages[name]["origin"] = v + [k] + message.get("origin", [])
-    # keep searching
-    for r in traits[k].requires.keys():
-        todo.insert((t, r, v + [k]), 0)
 
 
 # daemon ------------------------------------------------------------------------------------------
@@ -85,12 +56,7 @@ class Daemon(object):
         self.doc = kwargs["doc"]
         self.links = kwargs.get("links", dict())
         self.installation = kwargs.get("installation", dict())
-        self.traits = dict()
-        for t in kwargs["traits"]:
-            self.traits[t] = []
-            for tt in traits[t].requires:
-                if tt not in self.traits.keys():
-                    self.traits[tt] = [t] + traits[t].requires[tt]
+        self.traits = kwargs["traits"]
         self.config = kwargs.get("config", dict())
         self.state = kwargs.get("state", dict())
         self.messages = kwargs.get("messages", dict())
@@ -99,10 +65,8 @@ class Daemon(object):
     def __repr__(self):
         return self.name
 
-    def format_origin(self, lis):
-        return "from " + " ➜ ".join(
-            [f"<a href='../../traits/{l}'>{l}</a>" for l in lis]
-        )
+    def format_origin(self, origin):
+        return f"from <a href='../../traits/{origin}'>{origin}</a>"
 
 
 daemons = {}
@@ -119,25 +83,8 @@ daemons = collections.OrderedDict(sorted(daemons.items()))
 
 # back-populate
 for d in daemons.values():
-    for t, v in d.traits.items():
-        traits[t].daemons[d.name] = v
-
-# populate
-for d in daemons.values():
-    for t, v in d.traits.items():
-        for c in traits[t].config.keys():
-            if c not in d.config.keys():
-                d.config[c] = copy.deepcopy(traits[t].config[c])
-            d.config[c].update(copy.deepcopy(traits[t].config[c]))
-            d.config[c]["origin"] = [t] + d.config[c].get("origin", [])
-        for s in traits[t].state.keys():
-            if s not in d.state.keys():
-                d.state[s] = copy.deepcopy(traits[t].state[s])
-                d.state[s]["origin"] = [t] + d.state[s].get("origin", [])
-        for m in traits[t].messages.keys():
-            if m not in d.messages.keys():
-                d.message[m] = copy.deepcopy(traits[t].message[m])
-                d.message[m]["origin"] = [t] + d.message[m].get("origin", [])
+    for t in d.traits:
+        traits[t].daemons.append(d.name)
 
 
 # hardware ----------------------------------------------------------------------------------------
